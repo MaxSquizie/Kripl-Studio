7afa7d4230c0f0b0d843da443b47efd8f3329369
MaxSquizie <ilya.savostov@bk.ru>
2026-09-18 17:29:29 +0300

    feat(pi): export permission gate writer

--- packages/pi-adapter/src/index.ts
+++ packages/pi-adapter/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./pi-agent-runtime.js";
 export * from "./pi-event-normalizer.js";
 export * from "./pi-local-config.js";
+export * from "./pi-permission-gate.js";
 export * from "./rpc-process.js";
